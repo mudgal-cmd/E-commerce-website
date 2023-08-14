ff14878befff90135c89e84c170048bcee4a121d
mudgal-cmd <nishant2020.mudgal@gmail.com>
2023-08-14 11:09:22 +0530

    Added the data/product.js file

--- scripts/website.js
+++ scripts/website.js
@@ -1,59 +1,68 @@
 console.log('Hello');
 
-const products = [{
-  product_image : 'images/products/athletic-cotton-socks-6-pairs.jpg',
-  product_name : 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-  product_rating : {
-    stars : 4.5,
-    count : 87
-  },
-  // Since JS has problem working with float, so saving the product price in cents, the price below actually is $10.90
-  product_priceCents : 1090 
-},
-{
-  product_image : 'images/products/intermediate-composite-basketball.jpg',
-  product_name: 'Intermediate Size Basketball',
-  product_rating: {
-    stars : 0.4,
-    count : 127
-  },
-  product_priceCents : 2095
-},
-{
-  product_image : 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-  product_name : 'Adults Plain Cotton T-Shirt - 2 Pack',
-  product_rating: {
-    stars : 4.5,
-    count : 56
-  },
-  product_priceCents : 799
-}];
+// const products = [{
+//   product_image : 'images/products/athletic-cotton-socks-6-pairs.jpg',
+//   product_name : 'Black and Gray Athletic Cotton Socks - 6 Pairs',
+//   product_rating : {
+//     stars : 4.5,
+//     count : 87
+//   },
+//   // Since JS has problem working with float, so saving the product price in cents, the price below actually is $10.90
+//   product_priceCents : 1090 
+// },
+// {
+//   product_image : 'images/products/intermediate-composite-basketball.jpg',
+//   product_name: 'Intermediate Size Basketball',
+//   product_rating: {
+//     stars : 0.4,
+//     count : 127
+//   },
+//   product_priceCents : 2095
+// },
+// {
+//   product_image : 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
+//   product_name : 'Adults Plain Cotton T-Shirt - 2 Pack',
+//   product_rating: {
+//     stars : 4.5,
+//     count : 56
+//   },
+//   product_priceCents : 799
+// },
+// {
+//   product_image : 'images/products/black-2-slot-toaster.jpg',
+//   product_name : '2 Slot Toster - Black',
+//   product_rating : {
+//     stars : 0.5,
+//     count : 2197
+//   },
+//   product_priceCents : 1899
+// }];
 
 // console.log(products);
 let productsHTML = '';
 products.forEach((product)=>{
-  console.log(product.product_image);
+  console.log(product.image);
   const html = `
     <div class="product-container">
     <div class="product-image-container">
       <img class="product-image"
-        src="${product.product_image}">
+        src="${product.image}">
     </div>
 
     <div class="product-name limit-text-to-2-lines">
-      ${product.product_name}
+      ${product.name}
     </div>
 
     <div class="product-rating-container">
       <img class="product-rating-stars"
-        src="images/ratings/rating-${product.product_rating.stars*10}.png">
+        src="images/ratings/rating-${product.rating.stars*10}.png">
       <div class="product-rating-count link-primary">
-        ${product.product_rating.count}
+        ${product.rating.count}
       </div>
     </div>
 
     <div class="product-price">
-      ${(product.product_priceCents/100).toFixed(2)}
+      ${(product.priceCents/100).toFixed(2)}
     </div>
 
     <div class="product-quantity-container">
@@ -84,7 +93,7 @@ products.forEach((product)=>{
   </div>`;
 
   // console.log(html);
-  productsHTML+=html
+  productsHTML+=html;
 });
 
 console.log(productsHTML);
